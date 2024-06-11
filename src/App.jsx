410b61cd4a76a35faab0446fb21aac36999815e2
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-06-11 12:36:54 +0530

    Installed shadcn and react router and made necessary configurations

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-
-import './App.css'
+import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
       <h1>testing</h1>
+      <Button>Click</Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
