9cf42e936be743c1b9756bd79974a0031818b15f
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-06-11 14:07:50 +0530

    Testing completed....app is working fine

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ import { Button } from "./components/ui/button";
 function App() {
   return (
     <>
-      <h1>testing</h1>
-      <Button>Click</Button>
+      <div className="testing">
+        <h1>testing</h1>
+        <Button variant="blue">Click</Button>
+      </div>
     </>
   );
 }
