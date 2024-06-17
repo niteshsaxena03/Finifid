b8ed2c73efe80b83ba1c3e64b50c670035ee5888
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-06-17 16:37:46 +0530

    made some changes in friend screen

--- src/pages/Friends/FollowingList.jsx
+++ src/pages/Friends/FollowingList.jsx
@@ -10,6 +10,14 @@ function FollowingList() {
         <li>Following 1</li>
         <li>Following 2</li>
         <li>Following 3</li>
+        <li>Following 4</li>
+        <li>Following 5</li>
+        <li>Following 6</li>
+        <li>Following 7</li>
+        <li>Following 8</li>
+        <li>Following 9</li>
+        <li>Following 10</li>
+        <li>Following 11</li>
       </ul>
     </div>
   );
