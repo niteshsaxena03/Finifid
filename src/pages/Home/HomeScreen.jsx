39beef1a6068080cf834de2e15383a71b6baa676
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-06-18 18:53:53 +0530

    made some changes in styling of friend screen

--- src/pages/Home/HomeScreen.jsx
+++ src/pages/Home/HomeScreen.jsx
@@ -2,6 +2,7 @@ import Header from "../../components/Navbar/header.jsx";
 import Sidebar from "../../components/Navbar/sidebar.jsx";
 import Feed from "../../components/Feed/feed.jsx";
 import RightBar from "../../components/RightBar/rightBar.jsx";
+import "./HomeScreen.css";
 
 function HomeScreen() {
   return (
