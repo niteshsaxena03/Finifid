9f73f48b92194d84b60757eb3114a08f1c3f5982
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-06-25 17:02:23 +0530

    Changed heading of stories for user

--- src/components/Story/UserStory.jsx
+++ src/components/Story/UserStory.jsx
@@ -1,45 +1,39 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react';
+import { useEffect } from "react";
+import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { getDownloadURL, listAll, ref, uploadBytes } from "firebase/storage";
-import { storage } from '../../Firebase/firebaseContext';
-import { useNavigate } from 'react-router-dom';
+import { storage } from "../../Firebase/firebaseContext";
+import { useNavigate } from "react-router-dom";
 
 // css
-import './Story.css'
+import "./Story.css";
 
+// Icon
+import { Avatar } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
 
-// Icon 
-import { Avatar } from '@mui/material'
-import AddIcon from '@mui/icons-material/Add';
-
-const UserStory = ({UserData}) => {
-
-
-   // Photo Work 
+const UserStory = ({ UserData }) => {
+  // Photo Work
 
   let [img, setImg] = useState("");
   let [imgurl, setImgUrl] = useState([]);
-  let [isStorySet , setStory ] = useState(false) ;
+  let [isStorySet, setStory] = useState(false);
 
   const navigate = useNavigate();
 
-
   const seeStory = () => {
-    navigate(`/story`);  // Updated method
+    navigate(`/story`); // Updated method
   };
 
-
-  useEffect(()=>{
-    if(UserData.isStory==true){
-        let container = document.querySelector(".userContainer .MuiAvatar-root")
-        container.classList.add("makeClick")   ;
+  useEffect(() => {
+    if (UserData.isStory == true) {
+      let container = document.querySelector(".userContainer .MuiAvatar-root");
+      container.classList.add("makeClick");
     }
-  },[])
+  }, []);
 
   const addStory = async (event) => {
-
-    console.log("work") ;
+    console.log("work");
     const file = event.target.files[0];
     if (!file) return;
 
@@ -47,57 +41,50 @@ const UserStory = ({UserData}) => {
 
     const imgRef = ref(storage, `Story/${uuidv4()}`);
     try {
-      
-    //   Upload process :
+      //   Upload process :
 
       await uploadBytes(imgRef, file);
       const url = await getDownloadURL(imgRef);
       setImgUrl((prevUrls) => [...prevUrls, url]);
-    
-      // after upload :    
-      let container = document.querySelector(".userContainer .MuiAvatar-root") ;
-      console.log(container) ;
 
-        
-      
-      //Show After  Upload : 
-      UserData.isStory = true ; 
-      container.classList.add("afterStoryPost")    
-      setStory(true) ;
-    
+      // after upload :
+      let container = document.querySelector(".userContainer .MuiAvatar-root");
+      console.log(container);
 
-
+      //Show After  Upload :
+      UserData.isStory = true;
+      container.classList.add("afterStoryPost");
+      setStory(true);
     } catch (error) {
       console.error("Error uploading image:", error);
     }
-
-
-
-
   };
 
-
-  
-
-
   return (
-    <div className="borderBox" style={{ '--bg-image': `url(${UserData.url})` }}>
-      <div className='storyIcon'>
-
-        <span className='avatarContainer userContainer' onClick={ ( UserData.isStory == true) ? seeStory : null  }>
+    <div className="borderBox" style={{ "--bg-image": `url(${UserData.url})` }}>
+      <div className="storyIcon">
+        <span
+          className="avatarContainer userContainer"
+          onClick={UserData.isStory == true ? seeStory : null}
+        >
           <Avatar src={UserData.url} />
 
           <label htmlFor="iconPlus">
-            <span className='plusIcon'>
+            <span className="plusIcon">
               <AddIcon />
             </span>
           </label>
-          <input type="file" style={{ display: "none" }} onChange={addStory} id='iconPlus' />
+          <input
+            type="file"
+            style={{ display: "none" }}
+            onChange={addStory}
+            id="iconPlus"
+          />
         </span>
-        <h4 className='storyFont'>{UserData.name}</h4>
+        <h4 className="storyFont">{UserData.name}</h4>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default UserStory        
+export default UserStory;
