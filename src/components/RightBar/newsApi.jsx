2fcd4e309b078d52cf038fb509953a637a6a1482
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-07-11 22:46:39 +0530

    Merged

--- src/components/RightBar/newsApi.jsx
+++ src/components/RightBar/newsApi.jsx
@@ -1,6 +1,6 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 const NewsApi = ({ end }) => {
   const [filterNews, setFilterNews] = useState([]);
@@ -8,10 +8,12 @@ const NewsApi = ({ end }) => {
   useEffect(() => {
     async function fetchNews() {
       try {
-        const news = (await axios.get("https://newsserver-bsja.onrender.com/api/news")).data;
+        const news = (
+          await axios.get("https://newsserver-bsja.onrender.com/api/news")
+        ).data;
         setFilterNews(news);
       } catch (error) {
-        console.error('Error fetching news:', error);
+        console.error("Error fetching news:", error);
       }
     }
 
@@ -19,27 +21,26 @@ const NewsApi = ({ end }) => {
   }, [end]); // Fetch news whenever `end` changes
 
   const conciseDescription = (text) => {
-    return text != null ? text.split(" ").slice(0, 3).join(" ") + " ..." : "Sorry! News Not Available";
+    return text != null
+      ? text.split(" ").slice(0, 3).join(" ") + " ..."
+      : "Sorry! News Not Available";
   };
 
   return (
-    
     <ul>
-      {
-      filterNews 
-      ?
-      filterNews.map((mp) => (
-        <li key={uuidv4()}>
-          <div className="newsBox">
-            <a href={mp.url}><h5 className="newsFont">{mp.title}</h5></a>
-            <p className="sideFont">{conciseDescription(mp.description)}</p>
-            <div className="sepLineNews" />
-          </div>
-        </li>
-      ))
-      : 
-      null 
-      }
+      {filterNews
+        ? filterNews.map((mp) => (
+            <li key={uuidv4()}>
+              <div className="newsBox">
+                <a href={mp.url}>
+                  <h5 className="newsFont">{mp.title}</h5>
+                </a>
+                <p className="sideFont">{conciseDescription(mp.description)}</p>
+                <div className="sepLineNews" />
+              </div>
+            </li>
+          ))
+        : null}
     </ul>
   );
 };
